d13a54b57972d9c2b16c07699908d869bc8c5056
nochyq <cynthian2@cpp.edu>
2024-11-01 17:50:42 -0700

    skeleton

---
+++ Room.java
@@ -0,0 +1,17 @@
+public class Room {
+    // Attributes
+    private String location;
+    private int roomNumber;
+
+    // Constructor
+    public Room(String location, int roomNumber) {
+        this.location = location;
+        this.roomNumber = roomNumber;
+    }
+
+    // Getter and Setter methods
+    public String getLocation() { return location; }
+    public int getRoomNumber() { return roomNumber; }
+    public void setLocation(String location) { this.location = location; }
+    public void setRoomNumber(int roomNumber) { this.roomNumber = roomNumber; }
+}
