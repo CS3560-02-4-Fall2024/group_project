2a3ac075594f9ae966b12be4874f7567395adc69
Caleb Chung <kaichun24@gmail.com>
2024-11-15 17:14:10 -0800

    fix font

--- frontend/src/pages/appointmentConfirmed.jsx
+++ frontend/src/pages/appointmentConfirmed.jsx
@@ -18,14 +18,14 @@ function AppointmentConfirmed() {
           <div className='w-[100vw] h-[88vh] bg-dg flex flex-col items-center'>
               {/* Main Display Card */}
               <div className="w-[32vw] h-[42vh] bg-g rounded-lg border-2 border-black mt-auto flex-col justify-center items-center ">
-                <p className="font-serif text-6xl text-white justify-self-center mt-8">Your Appointment</p>
-                <p className="font-serif text-6xl text-white justify-self-center">has been booked!</p>
-                <p className="text-white font-serif text-4xl justify-self-center mt-24">Appt.Time:</p>
-                <p className="font-serif text-white text-4xl justify-self-center mb-4">{date} at {time}.</p>
+                <p className="text-6xl font-bold text-white justify-self-center mt-8">Your Appointment</p>
+                <p className="text-6xl font-bold text-white justify-self-center">has been booked!</p>
+                <p className="text-white font-semibold text-4xl justify-self-center mt-24">Appt.Time:</p>
+                <p className="text-white font-semibold text-4xl justify-self-center mb-4"><em>{date}</em> at <em>{time}</em>.</p>
               </div>
 
               {/* Back to Home Button */}
-              <button className="w-[14vw] h-[7vh] bg-g hover:bg-[#587354] rounded-md border-2 border-black mt-32 mb-24 text-2xl font-mono">Back to Home</button>
+              <button className="w-[14vw] h-[7vh] bg-g hover:bg-[#587354] rounded-md border-2 border-black mt-32 mb-24 text-2xl text-white font-semibold">Back to Home</button>
           </div>
       </>
     )
