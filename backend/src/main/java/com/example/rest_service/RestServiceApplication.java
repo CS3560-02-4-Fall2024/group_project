342ccaa2ab48796b6b31eb277cd7ac0becef8926
Sam Ly <sam@samly.me>
2024-11-20 13:35:03 -0800

    convert project to rest

--- backend/src/main/java/com/example/rest_service/RestServiceApplication.java
+++ backend/src/main/java/com/example/rest_service/RestServiceApplication.java
@@ -1,13 +1,13 @@
-package com.group4.group_project;
+package com.example.rest_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroupProjectApplication {
+public class RestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GroupProjectApplication.class, args);
+		SpringApplication.run(RestServiceApplication.class, args);
 	}
 
 }
