0be101cdb3d89f6c42bd91eea32cb5e1a84d3d92
Sam Ly <sam@samly.me>
2024-11-26 00:45:25 -0800

    clean imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import DentistHome from './pages/dentistHome.jsx';
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
