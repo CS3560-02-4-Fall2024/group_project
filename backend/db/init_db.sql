bfc88f02063d50b3c35c218cfc0c415312f86bdf
Sam Ly <sam@samly.me>
2024-12-06 01:33:37 -0800

    update db schema

--- backend/db/init_db.sql
+++ backend/db/init_db.sql
@@ -36,14 +36,12 @@ CREATE TABLE
     FOREIGN KEY (officeId) REFERENCES offices (id)
   );
 
--- This shit wrong lmao
 CREATE TABLE
   availability (
     id INT AUTO_INCREMENT PRIMARY KEY, -- Unique identifier for availability slots
     dentistId INT NOT NULL,
-    startTime DATETIME NOT NULL,
-    endTime DATETIME NOT NULL,
-    status ENUM ('available', 'unavailable') NOT NULL,
+    timeSlot DATETIME NOT NULL,
+    status ENUM ('available', 'unavailable') NOT NULL DEFAULT 'available',
     FOREIGN KEY (dentistId) REFERENCES dentists (id)
   );
 
@@ -53,8 +51,7 @@ CREATE TABLE
     patientId INT NOT NULL,
     dentistId INT NOT NULL,
     status VARCHAR(150) NOT NULL,
-    start DATETIME NOT NULL,
-    end DATETIME NOT NULL,
+    timeSlot DATETIME NOT NULL,
     purpose VARCHAR(255),
     FOREIGN KEY (patientId) REFERENCES patients (id),
     FOREIGN KEY (dentistId) REFERENCES dentists (id)
